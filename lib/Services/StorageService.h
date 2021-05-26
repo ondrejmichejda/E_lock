67c6f2429c64d299d1d5219b94d620b14529fd24
Ondrej Michejda <ondrej.michejda@buhlergroup.com>
2021-05-26 15:20:06 +0200

    update

--- lib/Services/StorageService.h
+++ lib/Services/StorageService.h
@@ -1,13 +1,33 @@
+/*
+    StorageService.h - Stores data to SD memory card.
+*/
+
 #ifndef StorageService_h
 #define StorageService_h
+
+#include "SPI.h"
+#include "SD.h"
+
+#define sd_CS_pin 10    //Hard defined communication pin, pin is on shield module
 
 class StorageService : public BaseService, ILoggable{
 
 private:
     TimeService* _timeService;
+    bool _sdOk = false;
 
     //! Initialization.
     void _init(){
+        // check sd card connection
+        // if (!SD.begin(sd_CS_pin)) {
+        //     char failSD[] = "Missing or malfunction of SD card.";
+        //     Logger::Log(_timeService->TimeAct, this, failSD);
+        //     return;
+        // }
+        _sdOk = true;
+
+        char folder[] = "abc";
+        // CreateFolder(folder);
         Logger::Log(_timeService->TimeAct, this, initText);
     }
 
@@ -25,6 +45,13 @@ public:
     StorageService(TimeService* timeService){
         _timeService = timeService;
         _init();
+    }
+
+    //! Creates folder if not exists
+    //! @param fname Name of folder
+    void CreateFolder(char* fname){
+        if(_sdOk)
+            SD.mkdir(fname);
     }
 
     String GetLogName(){
